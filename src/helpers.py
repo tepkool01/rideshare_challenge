f7f98effb588dd74fa84c48b2495c4509d9e4568
Michael Young <mvy1221@gmail.com>
2022-03-12 22:38:50 -0800

    minor refactor to helpers

--- src/helpers.py
+++ src/helpers.py
@@ -3,6 +3,24 @@ from city import City
 from car import Car
 
 
+def _find_closest_passenger(current_position: Position, pickup_positions: list[Position]) -> Position:
+    if len(pickup_positions) == 0:
+        return current_position
+
+    closest_request = pickup_positions[0]
+    shortest_distance = get_path_distance(current_position, closest_request)
+    for pickup_request in pickup_positions[1:]:
+        if get_path_distance(current_position, pickup_request) < shortest_distance:
+            closest_request = pickup_request
+            shortest_distance = get_path_distance(current_position, pickup_request)
+
+    return closest_request
+
+
+def get_path_distance(pos1: Position, pos2: Position):
+    return abs(pos1.x - pos2.x) + abs(pos1.y - pos2.y)
+
+
 def print_status(city: City, car: Car):
     pickup_requests = city.get_pickup_requests()
     print(f"{len(pickup_requests)} outstanding requests.")
--- src/path_simple.py
+++ src/path_simple.py
@@ -1,3 +1,4 @@
+from helpers import _find_closest_passenger
 from interfaces.path_algorithm_interface import PathAlgorithmInterface
 from datatypes.position import Position
 from datatypes.passenger import Passenger
@@ -58,19 +59,3 @@ class PathSimple(PathAlgorithmInterface):
         return best_path
 
 
-def _find_closest_passenger(current_position: Position, pickup_positions: list[Position]) -> Position:
-    if len(pickup_positions) == 0:
-        return current_position
-
-    closest_request = pickup_positions[0]
-    shortest_distance = get_path_distance(current_position, closest_request)
-    for pickup_request in pickup_positions[1:]:
-        if get_path_distance(current_position, pickup_request) < shortest_distance:
-            closest_request = pickup_request
-            shortest_distance = get_path_distance(current_position, pickup_request)
-
-    return closest_request
-
-
-def get_path_distance(pos1: Position, pos2: Position):
-    return abs(pos1.x - pos2.x) + abs(pos1.y - pos2.y)
